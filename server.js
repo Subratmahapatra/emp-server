6b8bc880bb41a914ed2cbf0a327b16bf0a5b171d
SubratMahapatra <subratmahapatra935@gmail.com>
2026-05-18 12:06:13 +0530

    use express instead of json-server

--- server.js
+++ server.js
@@ -1,22 +1,49 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const express = require('express')
+const cors = require('cors')
+const fs = require('fs')
+const app = express()
 
-server.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200)
-  }
-  next()
+app.use(cors())
+app.use(express.json())
+
+// Read db.json
+const getDB = () => JSON.parse(fs.readFileSync('./db.json', 'utf8'))
+const saveDB = (data) => fs.writeFileSync('./db.json', JSON.stringify(data, null, 2))
+
+// GET all employees
+app.get('/employees', (req, res) => {
+  const db = getDB()
+  res.json(db.employees)
 })
 
-server.use(middlewares)
-server.use(router)
+// POST add employee
+app.post('/employees', (req, res) => {
+  const db = getDB()
+  const newEmployee = { id: Date.now(), ...req.body }
+  db.employees.push(newEmployee)
+  saveDB(db)
+  res.json(newEmployee)
+})
+
+// PUT update employee
+app.put('/employees/:id', (req, res) => {
+  const db = getDB()
+  const id = Number(req.params.id)
+  db.employees = db.employees.map(e => e.id === id ? { ...e, ...req.body } : e)
+  saveDB(db)
+  res.json(db.employees.find(e => e.id === id))
+})
+
+// DELETE employee
+app.delete('/employees/:id', (req, res) => {
+  const db = getDB()
+  const id = Number(req.params.id)
+  db.employees = db.employees.filter(e => e.id !== id)
+  saveDB(db)
+  res.json({ message: 'Deleted successfully' })
+})
 
 const PORT = process.env.PORT || 3001
-server.listen(PORT, () => {
-  console.log(`JSON Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
